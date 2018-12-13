b5a88432fe443ceecd06f29aa428e3a1ff635230
Boris Divjak <boris@divjak.net>
2018-12-13 01:42:43 +0000

    Update gstt-new-assessment-iframe-addon.js

--- assets/js/gstt-new-assessment-iframe-addon.js
+++ assets/js/gstt-new-assessment-iframe-addon.js
@@ -1,9 +1,4 @@
 $(function() {
-//  $("#patient-details-iframe").on("load", function() {
-//    $("#patient-details-iframe").contents().find("body, .card .card-header").css("background-color", "#005eb8");
-//    $("#patient-details-iframe").contents().find(".card-header-tabs .nav-link:not(.active)").css("color", "white");  
-//  });
-
   function extendIframe() {
     $("#patient-details-section").addClass("extended");
     $("#patient-details-section").removeClass("collapsed");
@@ -23,6 +18,8 @@ $(function() {
     }
   });
 
-  var iframeWindow = $("#patient-details-iframe").get(0).contentWindow;
-  if (iframeWindow) $(iframeWindow).on("focus", function() { extendIframe(); })
+  $("#patient-details-iframe").on("load", function() {
+    var iframeWindow = $("#patient-details-iframe").get(0).contentWindow;
+    if (iframeWindow) $(iframeWindow).on("focus", function() { extendIframe(); })
+  });
 });
