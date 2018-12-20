66c420cca00c178eb146ae23dee28005e94f32a3
Boris Divjak <boris@divjak.net>
2018-12-20 13:09:05 +0000

    fix prom popup to work on touch

--- assets/js/prom.js
+++ assets/js/prom.js
@@ -3,6 +3,11 @@ $(function() {
         return false;
     });
 
+    $(".prom-scale button").on("touchstart touchend", function(e) {
+        e.preventDefault();
+        $(this).toggleClass("popout");
+    });
+
     $(".prom-scale button").on("click", function() {
       $(this).addClass("active");
     });
