229e4a7ede53eb7dc3c489838dfe02a46159b436
Boris Divjak <boris@divjak.net>
2018-12-20 16:16:36 +0000

    Update prom.js

--- assets/js/prom.js
+++ assets/js/prom.js
@@ -1,30 +1,31 @@
 $(function() { 
-    var touchvar = "";
+    
+    // The `touchedbutton` var is used to tell if the touchstart event was triggered on a button or some other element.
+    // This is necessary as iOs sometimes triggers a click even if the touchstart event wasn't registered on the button itself.
+    // We set touchedbutton to true by default - this way clicks are properly processed even on devices that don't support touchstart
+    var touchedbutton = true;
 
     $("form").submit(function() { 
         return false;
     });
 
-    // note: for some reason `$(".prom-scale button").on("touchstart"` 
-    // does not catch all touchstart events for a button on iOs ...
-    // but listening for touchstart events on document and then checkng the target works well
-//    $(document).on("mousedown", function(e) {
-//      if ($(e.target, ".prom-scale").hasClass('btn')) $(e.target).addClass("popout");
-//      if ($(e.target, ".prom-scale").hasClass('btn')) $(e.target).css("height", "100px");
-//    });
-
     $(document).on("touchstart", function(e) {
-      if ($(e.target, ".prom-scale").hasClass('btn')) $(e.target).addClass("popout");
-      touchvar = $(e.target).html();
+      if ($(e.target, ".prom-scale").hasClass('btn')) {
+        $(e.target).addClass("popout");
+        touchedbutton = true;
+      } else {
+        touchedbutton = false;
+      }
     });
 
-    $(document).on("touchend touchcancel mouseup mouseout", function(e) {
+    $(document).on("touchend touchcancel", function(e) {
       if ($(e.target, ".prom-scale").hasClass('btn')) $(e.target).removeClass("popout");
     });
 
     $(".prom-scale button").on("click", function() {
-      $(".prom-scale button").removeClass("active");
-      alert(touchvar);
-      $(this).addClass("active");
+      if (touchedbutton) {
+        $(".prom-scale button").removeClass("active");
+        $(this).addClass("active");
+      }
     });
 });
