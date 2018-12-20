07cccc243e0d774910a57af67f36b1876c637130
Boris Divjak <boris@divjak.net>
2018-12-20 13:39:50 +0000

    Update prom.js

--- assets/js/prom.js
+++ assets/js/prom.js
@@ -3,9 +3,12 @@ $(function() {
         return false;
     });
 
-    $(".prom-scale button").on("touchstart touchend", function(e) {
-//        e.preventDefault();
-        $(this).toggleClass("popout");
+    $(".prom-scale button").on("touchstart", function(e) {
+        $(this).addClass("popout");
+    });
+
+    $(".prom-scale button").on("touchend touchcancel mouseout", function(e) {
+        $(this).removeClass("popout");
     });
 
     $(".prom-scale button").on("click", function() {
