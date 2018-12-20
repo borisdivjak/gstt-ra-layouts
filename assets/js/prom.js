4d01905e20f9a88fde9b64252bb950c49a7745fc
Boris Divjak <boris@divjak.net>
2018-12-20 16:05:37 +0000

    Update prom.js

--- assets/js/prom.js
+++ assets/js/prom.js
@@ -1,4 +1,6 @@
 $(function() { 
+    var touchvar = "";
+
     $("form").submit(function() { 
         return false;
     });
@@ -6,13 +8,14 @@ $(function() {
     // note: for some reason `$(".prom-scale button").on("touchstart"` 
     // does not catch all touchstart events for a button on iOs ...
     // but listening for touchstart events on document and then checkng the target works well
-    $(document).on("mousedown", function(e) {
-      if ($(e.target, ".prom-scale").hasClass('btn')) $(e.target).addClass("popout");
-      if ($(e.target, ".prom-scale").hasClass('btn')) $(e.target).css("height", "100px");
-    });
+//    $(document).on("mousedown", function(e) {
+//      if ($(e.target, ".prom-scale").hasClass('btn')) $(e.target).addClass("popout");
+//      if ($(e.target, ".prom-scale").hasClass('btn')) $(e.target).css("height", "100px");
+//    });
 
     $(document).on("touchstart", function(e) {
       if ($(e.target, ".prom-scale").hasClass('btn')) $(e.target).addClass("popout");
+      touchvar = $(e.target).html();
     });
 
     $(document).on("touchend touchcancel mouseup mouseout", function(e) {
@@ -21,6 +24,7 @@ $(function() {
 
     $(".prom-scale button").on("click", function() {
       $(".prom-scale button").removeClass("active");
+      alert(touchvar);
       $(this).addClass("active");
     });
 });
